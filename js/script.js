92c83465f179cd508a781c8e198cbed8412654f4
altryne <git@alexw.me>
2012-03-24 04:06:23 +0200

    blblblblbllbb

--- js/script.js
+++ js/script.js
@@ -7,20 +7,28 @@ var game_of_profiles  = gop = {
     friends_hash : {},
     loaded_pics : 0,
     friends_count : 25,
+    friends_per_level : 20,
+    current_level : 1,
+    current_level_place : 0,
+
     init : function(){
 //        FB.getLoginStatus(function(response){
 //            $.publish('fb/status',response);
 //        });
+
         var box_height = box_width = 80;
         var box_count = Math.floor( ($(window).height() / box_height) * ($(window).width() / box_width));
 
         gop.friends_count = box_count;
+
         var tmpl = $('<div class="bg_pic"></div>');
         var tmpl_cont = $('<div id="bg_pics"></div>');
         for (var i = 0; i < box_count; i++) {
             tmpl_cont.append(tmpl.clone());
         }
-        $('#game').append(tmpl_cont);
+        $('#main').prepend(tmpl_cont);
+        //save cache of bg_pics
+        gop.data.$bg_pics = $('.bg_pic');
         gop.bindEvents();
     },
     bindEvents : function(){
@@ -28,15 +36,18 @@ var game_of_profiles  = gop = {
         $.subscribe('fb/connected',gop.connected);
         $.subscribe('fb/fetched_friends',gop.data.getPics);
         $.subscribe('fb/pic_loaded',gop.data.countPics);
+        $.subscribe('fb/pic_loaded',gop.ui.prepareBackground);
 
         //user actions pubsub
         $.subscribe('game/wrong',gop.ui.userWrong);
         $.subscribe('game/correct',gop.ui.userCorrect);
 
         $.subscribe('game/correct',function(){
+            $('#right').animate({'opacity':1},100).animate({'opacity':0},100);
             console.log('user was correct');
         });
         $.subscribe('game/wrong',function(){
+            $('#wrong').animate({'opacity':1},100).animate({'opacity':0},100);
             console.log('user was wrong');
         });
 
@@ -46,7 +57,7 @@ var game_of_profiles  = gop = {
             $('#start').off('click');
         });
 
-        $('#stage').on('click', '.name' ,function(){
+        $('#start_cont').on('click', '.name' ,function(){
             gop.ui.userChoice(this);
         });
 
@@ -91,7 +102,7 @@ gop.data = {
             var friend = keys[Math.floor(Math.random() * keys.length)];
             random_friends.push(gop.data.friends[friend].uid);
         }
-        var $bg_pics = $('.bg_pic');
+
         FB.api(
                 {
                     method:'fql.query',
@@ -102,9 +113,10 @@ gop.data = {
                     $.each(data,function(i,item){
                         var guid = guidGenerator();
                         gop.friends_hash[guid] = item.id;
-                        var $tmpl = $('<div class="prof_pic"><img src="" alt=""></div>');
+                        $.extend(gop.data.friends[item.id],item);
+                        var $img = $('<img src=""/>');
 
-                        $tmpl.find('img').attr(
+                        $img.attr(
                                 {'src':this.pic_crop.uri,
                                  'id': guid,
                                  'width':180,
@@ -114,16 +126,10 @@ gop.data = {
                                  'data-right':this.pic_crop.right
                                 });
 
-                        $tmpl.find('img').on('load',function() {
-                            $.publish('fb/pic_loaded');
-                            $cur_pic = $bg_pics.eq(i);
-                            $cur_pic.css('background-image', 'url(' + item.pic_crop.uri + ')');
-                            window.setTimeout(function () {
-                                $bg_pics.eq(i).addClass('loaded')
-                            }, Math.floor(Math.random() * 1500) + 20);
-
+                        $img.on('load',function() {
+                            $.publish('fb/pic_loaded',[i,item]);
                         });
-                        $("#pics").append($tmpl);
+//                        $("#pics").append($tmpl);
                     })
                 }
         );
@@ -180,25 +186,48 @@ gop.ui = {
             $.publish('fb/connected', status)
         }
     },
-    showOnePic:function () {
-        var rnd_num = Math.floor(Math.random() * $('#pics').find('.prof_pic').length);
-        var rnd_pic = $('#pics').find('.prof_pic').eq(rnd_num);
-        gop.ui.active_pic = rnd_pic.find('img').attr('id');
-        $('#stage').empty().append(rnd_pic);
+    prepareBackground : function(e,i,item){
 
-        var friend_id = gop.friends_hash[gop.ui.active_pic];
+        $cur_pic = gop.data.$bg_pics.eq(i);
+        $cur_pic.css('background-image', 'url(' + item.pic_crop.uri + ')');
+        $cur_pic.attr('id','friend'+item.id);
+        window.setTimeout(function () {
+            gop.data.$bg_pics.eq(i).addClass('loaded')
+        }, Math.floor(Math.random() * 1500) + 20);
+    },
+    showOnePic:function () {
+        if(gop.current_level_place >= gop.friends_per_level){
+            alert('DONE!');
+            return false;
+        }else{
+            gop.current_level_place++;
+        }
+
+        var friends_with_pics = Object.keys(gop.friends_hash);
+        var rnd_num = Math.floor(Math.random() * friends_with_pics.length);
+
+        gop.ui.active_friend = gop.data.friends[gop.friends_hash[friends_with_pics[rnd_num]]];
+        var pic_uri = gop.ui.active_friend.pic_crop.uri;
+        var $tmp_div = $('<div class="prof_pic"></div>');
+
+        $tmp_div.css('background-image','url('+ pic_uri +')');
+        $('#start_cont').empty().append($tmp_div);
+
+        var friend_id = gop.ui.active_friend.id;
         var friends = gop.data.randomFriends(3,friend_id);
         friends.push(friend_id);
         friends.shuffle();
         var tmpl = $('<div class="name"></div>');
+        var $tmpl_cont = $('<div class="names"></div>');
         for (var i = 0; i < friends.length; i++) {
             var friend = friends[i];
-            tmpl.clone().html(gop.data.friends[friend].name).data('id',friend).appendTo('#stage');
+            tmpl.clone().html((i + 1) + '.'+ gop.data.friends[friend].name).data('id',friend).appendTo($tmpl_cont);
         }
+        $tmpl_cont.appendTo('#start_cont');
+
     },
     userChoice : function(elm){
-
-        if(gop.friends_hash[gop.ui.active_pic] == $(elm).data('id')){
+        if(gop.ui.active_friend.id == $(elm).data('id')){
             $.publish('game/correct');
         }else{
             $.publish('game/wrong');
